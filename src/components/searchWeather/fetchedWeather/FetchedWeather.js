ec972e178bec5cd94ce2e65039ccaa771c7f5d8e
scsaralfred <alfredoscarano87@gmail.com>
2018-01-20 16:21:54 +0800

    No hyohen

--- src/components/searchWeather/fetchedWeather/FetchedWeather.js
+++ src/components/searchWeather/fetchedWeather/FetchedWeather.js
@@ -3,13 +3,18 @@ import "./FetchedWeather.css"
 import WeatherIcon from "./weatherIcons/WeatherIcon"
 
 const fetchedWeather = (props) => {
+
+    var str = props.weatherCondition;
+    str = str.replace(/-/g, ' ');
+
+
     return (
         <div className="FetchedBlock">
             <h1>It's currently &nbsp;<strong>{props.temp.toFixed(0)}&#176;</strong> degrees in &nbsp;<strong>{props.city}</strong> .</h1>
             <div className="IconBlock">
                   <WeatherIcon weatherIcons={props.weatherCondition}/>
                   <div className="WeatherDescription">
-                        <p>{props.weatherCondition}</p>
+                        <p>{str}</p>
                    </div>
             </div>
         </div>
