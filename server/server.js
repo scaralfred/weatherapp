4000a50ff2c71dcff588af2c2a406f7d1037cdd7
scsaralfred <alfredoscarano87@gmail.com>
2018-01-21 13:45:48 +0800

    Setup for heroku and mongo activation

--- server/server.js
+++ server/server.js
@@ -4,19 +4,17 @@ const path = require('path');
 const app = express();
 const axios = require('axios')
 const weather = require('./weather');
+const { ObjectID } = require('mongodb');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
 }
 
+const port = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
@@ -30,10 +28,91 @@ app.post('/weather', function (req, res) {
     });
 });
 
+app.post('/todos', (req, res) => {
 
-console.log ('Server has started on port 8080');
+    console.log(req.body);
 
-app.listen(process.env.PORT || 8080);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({
+            todo
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({
+            todo
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.listen(port, () => {console.log(`Started up at port ${port}`)});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 
 
